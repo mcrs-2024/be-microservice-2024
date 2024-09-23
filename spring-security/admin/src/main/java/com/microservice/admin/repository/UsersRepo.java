bc3bd418c56cc91419aaca7ed9104697a335c3d1
lebaodangtv <141621199+lebaodangtv@users.noreply.github.com>
2024-09-23 16:07:08 +0700

    update

--- spring-security/admin/src/main/java/com/microservice/admin/repository/UsersRepo.java
+++ spring-security/admin/src/main/java/com/microservice/admin/repository/UsersRepo.java
@@ -16,17 +16,17 @@ import com.microservice.admin.entitys.Users;
 public interface UsersRepo extends JpaRepository<Users, String> {
 	
 	Users findByUsername(String username);
-	// lấy ra các user còn tồn tại chưa bị xóa
+
 	List<Users> findByIsDeleted(Boolean isDeleted);
-	// delete user
+
 	@Modifying(clearAutomatically = true)
 	@Query(value = "UPDATE users SET isDeleted = 1 WHERE username = ?", nativeQuery = true)
 	void deleteLogical(String username);	
-	// update tài khoản
+
 	@Modifying(clearAutomatically = true)
 	@Query(value = "UPDATE users SET email = ?1, hashPassword = ?2, fullname =?3 WHERE username = ?4", nativeQuery = true)
 	void update(String email, String hashPassword, String fullname, String username);
-	// update ko có password
+
 	@Modifying(clearAutomatically = true)
 	@Query(value = "UPDATE users SET email = ?1, fullname = ?2 WHERE username = ?3 ", nativeQuery = true)
 	void updateNonPass(String email, String fullname, String username);
